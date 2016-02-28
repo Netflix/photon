31b401041e498b58a05e1231b67d29bf6ae00bb3
Sreeram Chakrovorthy <schakrovorthy@netflix.com>
2016-02-27 21:47:03 -0800

    Implementation of the equals() and hashCode() methods for the DOMNodeObjectModel.

--- src/main/java/com/netflix/imflibrary/imp_validation/cpl/DOMNodeObjectModel.java
+++ src/main/java/com/netflix/imflibrary/imp_validation/cpl/DOMNodeObjectModel.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.annotation.Nonnull;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -16,8 +18,10 @@ import java.util.Map;
  */
 public class DOMNodeObjectModel {
     /*See definitions of NodeType in package org.w3c.dom.Node*/
-    private final short nodeType;
+    @Nonnull
+    private final Short nodeType;
     /*Node's Local Name*/
+    @Nonnull
     private final String localName;
     /*List of child ElementDOMNodes*/
     private final List<DOMNodeObjectModel> childrenDOMNodes = new ArrayList<>();
@@ -25,6 +29,10 @@ public class DOMNodeObjectModel {
     private final Map<String, List<String>> fields = new LinkedHashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(DOMNodeObjectModel.class);
 
+    /**
+     * A constructor for the object model of a DOM Node.
+     * @param node the DOM Node whose object model is desired.
+     */
     public DOMNodeObjectModel(Node node){
         this.nodeType = node.getNodeType();
         this.localName = node.getLocalName();
@@ -53,16 +61,75 @@ public class DOMNodeObjectModel {
         }
     }
 
-    public boolean isEquals(DOMNodeObjectModel other){
-        /**
-         * Compare the number of children DOM nodes and fields
-         */
-        boolean result = true;
-        result &= (this.childrenDOMNodes.size() == other.childrenDOMNodes.size()) ? true : false;
-        result &= (this.fields.size() == other.fields.size()) ? true : false;
+    /**
+     * A getter for the DOM node type represented by this object model.
+     * @return a short representing the DOM Node type as defined in org.w3c.dom.Node
+     */
+    public short getNodeType(){
+        return this.nodeType;
+    }
 
+    /**
+     * A getter for the DOM Node local name represented by this object model.
+     * @return a string representing the DOM Node's local name field.
+     */
+    public String getLocalName(){
+        return this.localName;
+    }
 
+    /**
+     * A method to compare 2 DOMObjectNodeModel objects to verify if 2 DOM Nodes have the same
+     * content.
+     * @param other the node to compare with.
+     * @return boolean returns true if the 2 DOMNodeObjectModels have the same content.
+     */
+    @Override
+    public boolean equals(Object other){
 
-        return  result;
+        if(other == null
+                || !other.getClass().isAssignableFrom(this.getClass())){
+            return false;
+        }
+
+        DOMNodeObjectModel otherDOMNodeObjectModel = (DOMNodeObjectModel) other;
+
+        if(this.childrenDOMNodes.size() != otherDOMNodeObjectModel.childrenDOMNodes.size()){
+            return false;
+        }
+
+        if(this.fields.size() == otherDOMNodeObjectModel.fields.size()){
+            return false;
+        }
+
+        for(DOMNodeObjectModel children : this.childrenDOMNodes){
+            boolean intermediateResult = false;
+            for(DOMNodeObjectModel otherChildren : otherDOMNodeObjectModel.childrenDOMNodes){
+                if(otherChildren.getNodeType() == children.getNodeType()
+                        && otherChildren.getLocalName().equals(children.getLocalName())){
+                    intermediateResult |= children.equals(otherChildren);
+                }
+            }
+            if(!intermediateResult){
+                return false;
+            }
+        }
+
+        if(!this.fields.equals(otherDOMNodeObjectModel.fields)){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * A Java compliant implementation of the hashCode() method
+     * @return integer containing the hash code corresponding to this object
+     */
+    @Override
+    public int hashCode(){
+        int hash = 1;
+        hash = hash * 31 + this.localName.hashCode(); /*LocalName can be used since it is non-null*/
+        hash = hash * 31
+                + this.nodeType.hashCode();/*Another field that is indicated to be non-null*/
+        return hash;
     }
 }
