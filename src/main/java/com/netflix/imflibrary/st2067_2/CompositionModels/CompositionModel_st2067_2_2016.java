8b67f443913e73b3b0125821a49ed2ba960da0bd
Subbu Venkatrav <svenkatrav@netflix.com>
2016-08-05 18:50:00 -0700

    Adding classes for segment, sequence and composition

--- src/main/java/com/netflix/imflibrary/st2067_2/CompositionModels/CompositionModel_st2067_2_2016.java
+++ src/main/java/com/netflix/imflibrary/st2067_2/CompositionModels/CompositionModel_st2067_2_2016.java
@@ -26,6 +26,89 @@ public final class CompositionModel_st2067_2_2016
      * @param imfErrorLogger - an object for logging errors
      * @return a map containing mappings of a UUID to the corresponding VirtualTrack
      */
+    public static CompositionPlaylistType getCompositionPlayList (@Nonnull org.smpte_ra.schemas.st2067_2_2016.CompositionPlaylistType compositionPlaylistType, @Nonnull IMFErrorLogger imfErrorLogger)
+    {
+        List<SegmentType> segmentList = new ArrayList<SegmentType>();
+        for (org.smpte_ra.schemas.st2067_2_2016.SegmentType segment : compositionPlaylistType.getSegmentList().getSegment())
+        {
+            List<SequenceType> sequenceList = new ArrayList<SequenceType>();
+            org.smpte_ra.schemas.st2067_2_2016.SequenceType sequence;
+            for (Object object : segment.getSequenceList().getAny())
+            {
+                if(!(object instanceof JAXBElement)){
+                    imfErrorLogger.addError(IMFErrorLogger.IMFErrors.ErrorCodes.IMF_CPL_ERROR, IMFErrorLogger.IMFErrors.ErrorLevels.WARNING, "Unsupported sequence type or schema");
+                    continue;
+                }
+                JAXBElement jaxbElement = (JAXBElement)(object);
+                sequence = (org.smpte_ra.schemas.st2067_2_2016.SequenceType)(jaxbElement).getValue();
+                if (sequence != null)
+                {
+                    UUID uuid = UUIDHelper.fromUUIDAsURNStringToUUID(sequence.getTrackId());
+                    /**
+                     * A LinkedList seems appropriate since we want to preserve the order of the Resources referenced
+                     * by a virtual track to recreate the presentation. Since the LinkedList implementation is not
+                     * synchronized wrapping it around a synchronized list collection, although in this case it
+                     * is perhaps not required since this method is only invoked from the context of the constructor.
+                     */
+                    List<BaseResourceType> baseResources = Collections.synchronizedList(new LinkedList<>());
+                    for (org.smpte_ra.schemas.st2067_2_2016.BaseResourceType resource : sequence.getResourceList().getResource())
+                    {
+                        BaseResourceType baseResource;
+                        if(resource instanceof  org.smpte_ra.schemas.st2067_2_2016.TrackFileResourceType)
+                        {
+
+                            org.smpte_ra.schemas.st2067_2_2016.TrackFileResourceType trackFileResource =
+                                    (org.smpte_ra.schemas.st2067_2_2016.TrackFileResourceType) resource;
+
+                            baseResource = new TrackFileResource_st2067_2_2016(
+                                    trackFileResource.getId(),
+                                    trackFileResource.getTrackFileId(),
+                                    trackFileResource.getEditRate(),
+                                    trackFileResource.getIntrinsicDuration(),
+                                    trackFileResource.getEntryPoint(),
+                                    trackFileResource.getSourceDuration(),
+                                    trackFileResource.getRepeatCount(),
+                                    trackFileResource.getSourceEncoding(),
+                                    trackFileResource.getHashAlgorithm()
+                            );
+                        }
+                        else
+                        {
+                            baseResource = new BaseResourceType(resource.getId(),
+                                    resource.getEditRate(),
+                                    resource.getIntrinsicDuration(),
+                                    resource.getEntryPoint(),
+                                    resource.getSourceDuration(),
+                                    resource.getRepeatCount());
+                        }
+                        baseResources.add(baseResource);
+                    }
+                    sequenceList.add(new SequenceType(sequence.getId(), sequence.getTrackId(), baseResources));
+                }
+            }
+            sequenceList = Collections.unmodifiableList(sequenceList);
+            segmentList.add(new SegmentType(segment.getId(), sequenceList));
+        }
+        segmentList = Collections.unmodifiableList(segmentList);
+
+        CompositionPlaylistType cpl =  new CompositionPlaylistType( compositionPlaylistType.getId(),
+                compositionPlaylistType.getEditRate(),
+                (compositionPlaylistType.getAnnotation() == null ? null : compositionPlaylistType.getAnnotation().getValue()),
+                (compositionPlaylistType.getIssuer() == null ? null : compositionPlaylistType.getIssuer().getValue()),
+                (compositionPlaylistType.getCreator() == null ? null : compositionPlaylistType.getCreator().getValue()),
+                (compositionPlaylistType.getContentOriginator() == null ? null : compositionPlaylistType.getContentOriginator().getValue()),
+                (compositionPlaylistType.getContentTitle() == null ? null : compositionPlaylistType.getContentTitle().getValue()),
+                segmentList);
+
+        return cpl;
+    }
+    
+    /**
+     * A stateless method that reads and parses all the virtual tracks of a Composition
+     * @param compositionPlaylistType - a CompositionPlaylist object model
+     * @param imfErrorLogger - an object for logging errors
+     * @return a map containing mappings of a UUID to the corresponding VirtualTrack
+     */
     public static Map<UUID, VirtualTrack> getVirtualTracksMap (@Nonnull org.smpte_ra.schemas.st2067_2_2016.CompositionPlaylistType compositionPlaylistType, @Nonnull IMFErrorLogger imfErrorLogger)
     {
         Map<UUID, VirtualTrack> virtualTrackMap = new LinkedHashMap<>();
---
+++ src/main/java/com/netflix/imflibrary/st2067_2/CompositionPlaylistType.java
@@ -0,0 +1,124 @@
+/*
+ *
+ * Copyright 2015 Netflix, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package com.netflix.imflibrary.st2067_2;
+
+import javax.annotation.concurrent.Immutable;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * A class that models a Composition's segment structure.
+ */
+@Immutable
+public final class CompositionPlaylistType {
+    private final String id;
+    private final Composition.EditRate editRate;
+    private final String annotation;
+    private final String issuer;
+    private final String creator;
+    private final String contentOriginator;
+    private final String contentTitle;
+    protected final List<SegmentType> segmentList;
+
+    public CompositionPlaylistType(String id,
+                                   List<Long> editRate,
+                                   String annotation,
+                                   String issuer,
+                                   String creator,
+                                   String contentOriginator,
+                                   String contentTitle,
+                                   List<SegmentType> segmentList)
+    {
+        this.id                = id;
+        this.editRate          = new Composition.EditRate(editRate);
+        this.annotation        = annotation;
+        this.issuer            = issuer;
+        this.creator           = creator;
+        this.contentOriginator = contentOriginator;
+        this.contentTitle      = contentTitle;
+        this.segmentList        = segmentList;
+    }
+
+    /**
+     * Getter for the Sequence ID
+     * @return a string representing the urn:uuid of the segment
+     */
+    public String getId(){
+        return this.id;
+    }
+
+    /**
+     * Getter for the EditRate of the Composition
+     * @return a Composition.EditRate object of the Composition
+     */
+    public Composition.EditRate getEditRate(){
+        return this.editRate;
+    }
+
+    /**
+     * Getter for the Composition annotation
+     * @return a string representing annotation of the Composition
+     */
+    public String getAnnotation(){
+        return this.annotation;
+    }
+
+    /**
+     * Getter for the Composition issuer
+     * @return a string representing issuer of the Composition
+     */
+    public String getIssuer(){
+        return this.issuer;
+    }
+
+    /**
+     * Getter for the Composition creator
+     * @return a string representing creator of the Composition
+     */
+    public String getCreator(){
+        return this.creator;
+    }
+
+    /**
+     * Getter for the Composition contentOriginator
+     * @return a string representing contentOriginator of the Composition
+     */
+    public String getContentOriginator(){
+        return this.contentOriginator;
+    }
+
+    /**
+     * Getter for the Composition contentTitle
+     * @return a string representing contentTitle of the Composition
+     */
+    public String getContentTitle(){
+        return this.contentTitle;
+    }
+
+    /**
+     * Getter for the SegmentList
+     * @return a string representing the SegmentList of the composition
+     */
+    public List<SegmentType> getSegmentList(){
+        return this.segmentList;
+    }
+
+
+}
