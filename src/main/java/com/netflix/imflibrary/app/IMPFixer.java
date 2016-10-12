dd4f65f936b652d9ed1cdbea97e8667db8da4b35
Subbu Venkatrav <svenkatrav@netflix.com>
2016-10-12 14:10:29 -0700

    Fix IMPFixer help message

--- src/main/java/com/netflix/imflibrary/app/IMPFixer.java
+++ src/main/java/com/netflix/imflibrary/app/IMPFixer.java
@@ -44,7 +44,7 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 public class IMPFixer {
 
     private static final String CONFORMANCE_LOGGER_PREFIX = "Virtual Track Conformance";
-    private static final Logger logger = LoggerFactory.getLogger(IMPAnalyzer.class);
+    private static final Logger logger = LoggerFactory.getLogger(IMPFixer.class);
 
     private static UUID getTrackFileId(PayloadRecord headerPartitionPayloadRecord) throws
             IOException {
@@ -290,7 +290,7 @@ public class IMPFixer {
     private static String usage() {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("Usage:%n"));
-        sb.append(String.format("%s input_package_directory output_package_directory [options]%n", IMPAnalyzer.class.getName()));
+        sb.append(String.format("%s input_package_directory output_package_directory [options]%n", IMPFixer.class.getName()));
         sb.append(String.format("options:            %n"));
         sb.append(String.format("-cs, --cpl-schema=VERSION      CPL schema version for output IMP, supported values are 2013 or 2016%n"));
         sb.append(String.format("-nc, --no-copy                 don't copy track files     %n"));
